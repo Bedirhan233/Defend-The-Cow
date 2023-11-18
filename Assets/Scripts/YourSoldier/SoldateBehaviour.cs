1e7174b74803f2abfc6ef8c0b9a60ff5c2b32b06
Bedirhan233 <bedirhanko73@gmail.com>
2023-11-18 20:15:07 +0100

    Raycast

--- Assets/Scripts/YourSoldier/SoldateBehaviour.cs
+++ Assets/Scripts/YourSoldier/SoldateBehaviour.cs
@@ -9,10 +9,13 @@ public class SoldateBehaviour : MonoBehaviour
     Vector3 velocity;
     Vector2 direction;
 
+    Vector2[] dirs = { Vector2.up, Vector2.down, Vector2.right, Vector2.left };
+
     GameObject target;
 
     bool tooClose;
     float timer;
+    float timerForRandom;
 
     [Header ("UI")]
     public Image greenBar;
@@ -28,10 +31,11 @@ public class SoldateBehaviour : MonoBehaviour
     [Header("Move settings and range")]
     public bool canShoot;
     public bool inMoveRange;
-    public float range = 5;
+    public float startToIdle = 5;
     public float shootingRange = 10;
     public float movingRange = 20;
     public float Movespeed = 5;
+    public float walkPointRange = 5;
 
     [Header("Health")]
     public float health = 100;
@@ -42,10 +46,15 @@ public class SoldateBehaviour : MonoBehaviour
     public float bulletDamage = 10;
     public float fireRate;
 
+    Vector2 walkPoint;
+
+    
+
 
     // Start is called before the first frame update
     void Start()
     {
+        Physics2D.queriesStartInColliders = false;
         bulletScript.speed = bulletSpeed;
         bulletScript.damage = bulletDamage;
 
@@ -56,6 +65,102 @@ public class SoldateBehaviour : MonoBehaviour
     void Update()
     {
         FindAndMoveToEnemy();
+
+        
+
+    }
+
+
+    private void FindAndMoveToEnemy()
+    {
+        target = GameObject.FindGameObjectWithTag("Enemy");
+        direction = target.transform.position - transform.position;
+        direction.Normalize();
+
+        RaycastHit2D inIdleRange;
+        inIdleRange = Physics2D.Raycast(transform.position, direction, startToIdle);
+        Debug.DrawRay(transform.position, direction * startToIdle, Color.blue);
+
+        RaycastHit2D inMoveRange;
+        inMoveRange = Physics2D.Raycast(transform.position, direction, movingRange);
+        Debug.DrawRay(transform.position, direction * movingRange, Color.yellow);
+
+        RaycastHit2D inShootingRange = Physics2D.Raycast(transform.position, direction, shootingRange);
+        Debug.DrawRay(transform.position, direction * shootingRange, Color.red);
+
+        if (inShootingRange)
+        {
+            canShoot = true;
+        }
+        else
+        {
+            canShoot = false;
+        }
+
+        ShootingProcess();
+        if (inIdleRange)
+        {
+            tooClose = true;
+        }
+        else
+        {
+            tooClose = false;
+        }
+
+
+        if (tooClose)
+        {
+            velocity = Vector2.zero;
+            transform.up = direction;
+        }
+        if (!tooClose && inMoveRange)
+        {
+            Debug.Log("In move range");
+            transform.up = direction;
+            velocity = direction * Movespeed * Time.deltaTime;
+            transform.position += velocity;
+
+        }
+        Debug.Log("Time: " + timerForRandom);
+        if (!inShootingRange && !inMoveRange)
+        {
+            timerForRandom -= Time.deltaTime;
+
+            if (timerForRandom < 0)
+            {
+                RandomDirection();
+            }
+        }
+            
+        
+
+
+    }
+
+    private void RandomDirection()
+    {
+        
+        
+            Debug.Log("Random IDle");
+            var oldDir = direction;
+            do
+            {
+                direction = dirs[Random.Range(0, dirs.Length)];
+            }
+            while (direction == oldDir);
+
+            velocity = direction * Movespeed * Time.deltaTime;
+            transform.position += velocity;
+
+
+        timerForRandom = Random.Range(0.5f, 2);
+    }
+
+    private void ShootingProcess()
+    {
+        
+
+        
 
         if (canShoot)
         {
@@ -70,53 +175,10 @@ public class SoldateBehaviour : MonoBehaviour
             timer += Time.deltaTime;
 
         }
-
-
     }
+   
 
-    private void FindAndMoveToEnemy()
-    {
-
-            target = GameObject.FindGameObjectWithTag("Enemy");
-
-            if(target == null)
-            {
-            target = GameObject.FindGameObjectWithTag("Idle");
-            }
-            
-
-            direction = target.transform.position - transform.position;
-
-            if (direction.sqrMagnitude < range)
-            {
-                tooClose = true;
-            }
-
-            else
-            {
-                tooClose = false;
-            }
-            if (tooClose)
-            {
-                velocity = Vector2.zero;
-            }
-            if (!tooClose && inMoveRange)
-            {
-
-                direction.Normalize();
-                transform.up = direction;
-                velocity = direction * Movespeed * Time.deltaTime;
-                transform.position += velocity;
-
-            }
-
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, range);   
-    }
+    
 
     private void OnCollisionEnter2D(Collision2D other)
     {
